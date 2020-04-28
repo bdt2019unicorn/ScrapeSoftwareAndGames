08a3d3979d4d2e2a531ee426a1ed87964a2754d8
Hoang Thanh Binh <51917268+bdt2019unicorn@users.noreply.github.com>
2020-04-28 13:03:28 +1200

    innitial the code

---
+++ ScrapeSoftwareAndGames/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ScrapeSoftwareAndGames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
